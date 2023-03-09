3a64cb34a32c3c90e0edc6252df61dc53c9b68e1
Valeri85 <valeriousmonk85@gmail.com>
2023-03-09 15:33:22 +0400

    add black theme for docs

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,11 +1,9 @@
+import { themes } from '@storybook/theming';
 import '../styles.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	// actions: { argTypesRegex: '^on[A-Z].*' },
+	docs: {
+		theme: themes.dark,
+	},
+};
