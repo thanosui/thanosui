7ceb584c1a865dfd75445814458a43bee3687a75
Mk <miriankakhidze@gmail.com>
2023-03-16 17:03:49 +0400

    fix sb

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -5,18 +5,18 @@ module.exports = {
     "./index.html",
     "src/**/*.{js,ts,jsx,tsx}",
     // include packages if not transpiling
-    // "../../packages/**/*.{js,ts,jsx,tsx}",
+    "../../packages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-        'bg-neutral': colors.neutral[500],
-        'bg-primary': colors.blue[500],
-        'bg-secondary': colors.purple[500],
-        'bg-success ': colors.green[500],
-        'bg-warning ': colors.yellow[500],
-        'bg-danger ': colors.red[500],
-    },
     extend: {
+      // colors: {
+      //   'bg-neutral': colors.neutral[500],
+      //   'bg-primary': colors.blue[500],
+      //   'bg-secondary': colors.purple[500],
+      //   'bg-success ': colors.green[500],
+      //   'bg-warning ': colors.yellow[500],
+      //   'bg-danger ': colors.red[500],
+      // },
     },
   },
   plugins: [],
