542fac2e64f486fdfb8b590ae666f2fb821ee1fa
Mk <miriankakhidze@gmail.com>
2023-03-27 17:48:47 +0400

    add simple modal style

--- packages/ui/src/Modal/Modal.tsx
+++ packages/ui/src/Modal/Modal.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react"
 import { VariantProps, tv } from "tailwind-variants"
+import { Button } from "../Button"
 
 const modal = tv({})
 
@@ -9,6 +10,43 @@ export interface ModalProps extends ModalVariantProps {
 
 }
 
-export const Modal: FC<ModalProps> = () => {
-    return <></>
+export const Modal: FC<ModalProps> = (props) => {
+    const { isOpen, title, onClose, children } = props
+
+    const modalStyles = isOpen ? "block" : "hidden"; 
+    return (
+        <div className={`${modalStyles} fixed z-10 inset-0 overflow-y-auto`}>
+            <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div className="fixed inset-0 transition-opacity">
+                    <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                </div>
+                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                                <svg className="h-6 w-6 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+                                </svg>
+                            </div>
+                            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+                                    {title}
+                                </h3>
+                                <div className="mt-2">
+                                    {children}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <Button onClick={onClose} type="button">
+                            Close
+                        </Button>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
