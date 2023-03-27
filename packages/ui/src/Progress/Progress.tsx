0e5c491530f2eda4e8b777e2a2f9af931a2c022c
Mk <miriankakhidze@gmail.com>
2023-03-27 17:50:31 +0400

    simple progress comp

--- packages/ui/src/Progress/Progress.tsx
+++ packages/ui/src/Progress/Progress.tsx
@@ -9,6 +9,15 @@ export interface ProgressProps extends ProgressVariantProps {
 
 }
 
-export const Progress: FC<ProgressProps> = () => {
-    return <></>
+export const Progress: FC<ProgressProps> = (props) => {
+    const { value = 30, max = 100 } = props
+    const progress = (value / max) * 100;
+    return <>
+        <div className="relative h-4 rounded-full overflow-hidden bg-gray-200">
+            <div
+                className="absolute top-0 left-0 h-full bg-blue-500"
+                style={{ width: `${progress}%` }}
+            />
+        </div>
+    </>
 }
